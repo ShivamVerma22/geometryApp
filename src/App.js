12f9b2fceefd782b662ab447d659a84e88c76862
Shivam Verma <vermashivam527@gmail.com>
2020-12-28 01:53:06 +0530

    two modules done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,71 @@
-import React from "react";
+import React, {useState} from "react";
 import "./styles.css";
+import quizSections from  "./quizSections"
 
 export default function App() {
+  const [angles, setInput] = useState([]);
+  const [message, setMessage] = useState("");
+  const [currentSelection, setSelection] = useState("1");
+  const [randomAngles, setRandomAngles] = useState([]);
+
+  function handleInput(e){
+    setInput(angles => [...angles, Number(e.target.value)]);
+  }
+
+  function handleClick() {
+    let total = angles.reduce((total, a) => total + a);
+    if(total === 180){
+      setMessage("You have correctly entered angles of a triangle")
+    } else {
+      setMessage("The angles you entered does not make a triangle")
+    }
+  }
+
+  function randomAngleGenerator() {
+      let one = Math.floor(Math.random() * 90) + 1
+      let two = Math.floor(Math.random() * 89) + 1
+      let three = 180 - (one + two);
+      let tempArr = [one, two, three];
+      setRandomAngles(randomAngles => [...randomAngles,...tempArr]);
+      console.log(randomAngles)
+  }
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Fun with Geometry</h1>
+      <h2>An interactive way to learn geometry</h2>
+      {
+        quizSections.map(section => {
+          return(
+            <button 
+              key={section.value}
+              style={{display: "block", margin: "10px auto"}}
+              onClick={() => setSelection(section.value)}
+            >
+              {section.message}
+            </button>
+          )
+        })
+      }
+      {
+        currentSelection === "1" ? 
+        <>
+          <input type="text" onBlur={handleInput} />
+          <input type="text" onBlur={handleInput} />
+          <input type="text" onBlur={handleInput} />
+          <button onClick={handleClick}>?</button>
+          <h2>{message}</h2>
+        </> : 
+        ""
+      }
+      {
+        currentSelection === "2" ?
+        <>
+          <h4>What will be the type of triangle having these angles?</h4>
+          <button onClick={randomAngleGenerator}>Play</button>
+        </> :
+        ""
+      }
     </div>
   );
 }
---
+++ src/quizSections.js
@@ -0,0 +1,12 @@
+const quizSections = [
+  {
+    message: "Is this a triangle?",
+    value: "1"
+  },
+  {
+    message: "Which Triangle is this?",
+    value: "2"
+  } 
+]
+
+export default quizSections
