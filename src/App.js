ca8e0254b84c50f4d3e74e0ed12bcb49a5bf6057
Shivam Verma <vermashivam527@gmail.com>
2020-12-29 02:14:48 +0530

    options and functionality in module2

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ export default function App() {
   const [angles, setInput] = useState([]);
   const [message, setMessage] = useState("");
   const [currentSelection, setSelection] = useState("1");
-  const [randomAngles, setRandomAngles] = useState([]);
+  const [angleString, setAngleString] = useState("");
+  const [triangle, setTriangle] = useState("");
+  const [triangleMessage, setTriangleMessage] = useState("");
 
   function handleInput(e){
     setInput(angles => [...angles, Number(e.target.value)]);
@@ -25,9 +27,40 @@ export default function App() {
       let one = Math.floor(Math.random() * 90) + 1
       let two = Math.floor(Math.random() * 89) + 1
       let three = 180 - (one + two);
-      let tempArr = [one, two, three];
-      setRandomAngles(randomAngles => [...randomAngles,...tempArr]);
-      console.log(randomAngles)
+      let maxAngle = Math.max(one, two, three)
+      calculateTriange(maxAngle)
+      setAngleString(`${one} , ${two} , ${three}`);
+      setTriangleMessage("")
+  }
+
+  function calculateTriange(maxAngle) {
+    if(maxAngle >= 90){
+      if(maxAngle === 90){
+        setTriangle("right")
+      } else {
+        setTriangle("obtuse")
+      }
+    } else {
+      setTriangle("acute")
+    }
+  }
+
+  function handleTriangleType(e) {
+    if(e.target.checked === true){
+      if(e.target.value === triangle){
+        setTriangleMessage(`${e.target.value} angled triangle is Correct answer 🤓`)
+      } else {
+        setTriangleMessage(`${e.target.value} angled triangle is Wrong answer 🙁`)
+      }
+    }
+  }
+
+  function setActiveChoice() {
+    if(triangleMessage){
+      return true
+    } else {
+      return false
+    }
   }
 
   return (
@@ -62,7 +95,26 @@ export default function App() {
         currentSelection === "2" ?
         <>
           <h4>What will be the type of triangle having these angles?</h4>
-          <button onClick={randomAngleGenerator}>Play</button>
+          <button onClick={() => randomAngleGenerator()}>Play</button>
+          <h3>{angleString}</h3>
+          { angleString ? 
+            <>
+              <input type="radio" id="acute" name="triangle" value="acute" onChange={handleTriangleType} 
+              disabled={setActiveChoice()} 
+              />
+              <label>Acute Angled Triangle</label>
+              <input type="radio" id="right" name="triangle" value="right" onChange={handleTriangleType} 
+              disabled={setActiveChoice()} 
+              />
+              <label>Right Angled Triangle</label>
+              <input type="radio" id="obtuse" name="triangle" value="obtuse" onChange={handleTriangleType} 
+              disabled={setActiveChoice()} 
+              />
+              <label>Obtuse Angled Triangle</label>
+            </> : 
+            ""
+          }
+          <h2>{triangleMessage}</h2>
         </> :
         ""
       }
